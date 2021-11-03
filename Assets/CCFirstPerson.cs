af56eaf70e390b140743468f1d75742e29574b49
fosskat1 <katiefos@usc.edu>
2021-11-02 19:41:47 -0700

    Added rigidbody controller

---
+++ Assets/CCFirstPerson.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCFirstPerson : MonoBehaviour
+{
+    public CharacterController controller;
+    private bool movementEnabled = true;
+    public Animator walkAnimation;
+    public float speed = 0.1f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(movementEnabled){
+            Vector3 direction = new Vector3(0f, 0f, 1f).normalized;
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        
+        if (hit.transform.tag == "Obstacle") 
+        {
+            Debug.Log($"WE HIT AN OBSTACLE: {hit.transform.name}");
+            walkAnimation.enabled = false;
+            movementEnabled = false;
+        }
+    }
+}
--- Assets/ThirdPersonMovement.cs
+++ Assets/ThirdPersonMovement.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
-    public CharacterController controller;
+    public Rigidbody rb;
 
-    public float speed = 0.1f;
+    public float speed = 1f;
+    public float maxVelocityChange = 10f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Vector3 direction = new Vector3(0f, 0f, 1f).normalized;
-        controller.Move(direction * speed * Time.deltaTime);   
+        Vector3 targetDirection = new Vector3(0f, 0f, 1f);
+        Vector3 targetVelocity = targetDirection * speed;
+
+        // Apply a force that attempts to reach our target velocity
+        Vector3 velocity = rb.velocity;
+        Vector3 velocityChange = (targetVelocity - velocity);
+        velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+        velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+        velocityChange.y = 0;
+
+        rb.AddForce(velocityChange, ForceMode.VelocityChange);
     }
 }
