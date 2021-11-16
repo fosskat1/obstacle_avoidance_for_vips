e50b7b54b13cffa83889611e3588085cac3e21b5
advaitrane <advaitr11@gmail.com>
2021-11-16 13:39:30 -0800

    removed car

--- Assets/ObjectDetection_YoloV4.cs
+++ Assets/ObjectDetection_YoloV4.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3adac034df347387b059104b631412577db108f175a275b2e7f5e82c66aca74f
-size 13163
+oid sha256:c2216233f35b84ad4c1031cea33fec41b7c2f9e9b427ded939defdd7066483a1
+size 9835
