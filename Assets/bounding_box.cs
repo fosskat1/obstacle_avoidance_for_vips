b0a7f2529f2e31e4cbfcd604aa123feed4c99a53
fosskat1 <katiefos@usc.edu>
2021-11-16 20:11:53 -0800

    formatted code better

--- Assets/bounding_box.cs
+++ Assets/bounding_box.cs
@@ -12,22 +12,7 @@ public class bounding_box : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 m_Min;
-        Vector3 m_Max;
-        Vector3 m_Size;
-        Vector3 m_Center;
-        //Fetch the Collider from the GameObject
-        m_Collider = GetComponent<Collider>();
-        //Fetch the center of the Collider volume
-        m_Center = m_Collider.bounds.center;
-        //Fetch the size of the Collider volume
-        m_Size = m_Collider.bounds.size;
-        //Fetch the minimum and maximum bounds of the Collider volume
-        m_Min = m_Collider.bounds.min;
-        m_Max = m_Collider.bounds.max;
         lineRenderer.useWorldSpace = true;
-
-
     }
 
     // Update is called before every frame
@@ -40,14 +25,16 @@ public class bounding_box : MonoBehaviour
         Vector3 screenPosMax = camera.WorldToScreenPoint(new Vector3(max.x, max.y, max.z));
         Debug.Log("target min: " + screenPos.x + ", " + screenPos.y +  "; target max: " + screenPosMax.x + ", " + screenPosMax.y);
 
-        Vector3 t = camera.ScreenToWorldPoint(screenPos);
-        Vector3 t1 = camera.ScreenToWorldPoint(screenPosMax);
+        RenderPixelLine(screenPos, screenPosMax);
+    }
+
+    void RenderPixelLine(Vector3 conePosMin, Vector3 conePosMax)
+    {
+        Vector3 t = camera.ScreenToWorldPoint(conePosMin);
+        Vector3 t1 = camera.ScreenToWorldPoint(conePosMax);
         Debug.Log(t + ", " + t1);
         lineRenderer.SetPosition(0, t);
         lineRenderer.SetPosition(1, t1);
-
-
-
     }
 
 
