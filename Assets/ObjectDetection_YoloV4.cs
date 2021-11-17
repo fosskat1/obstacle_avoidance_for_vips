6a3ec2f9381b53f1ad700021fec906c7a416df6b
advaitrane <advaitr11@gmail.com>
2021-11-17 01:28:19 -0800

    adding new objects

--- Assets/ObjectDetection_YoloV4.cs
+++ Assets/ObjectDetection_YoloV4.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:340cded80671cbd116e806a554c46921e0bdf1417c4037a94328083be8267607
-size 9813
+oid sha256:3376bee03274fdf1d2ed6e6efbc0dc5d62958009330f3b8bfec8937b35e1c92b
+size 9904
