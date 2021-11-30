016fcc005bb1a0265d157a7c72da8dedbba1b6b3
fosskat1 <katiefos@usc.edu>
2021-11-29 20:22:30 -0800

    showing camera on screen

--- Assets/depthTexture.cs
+++ Assets/depthTexture.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e795f6c36fdefc671934b39b7efc57ffdc3b63c9273396cad7db50ef56db9d39
-size 1634
+oid sha256:759b7eeef10db1e323e7fc3a245a283b7562eacd98a6f1da207a387b2ad1db1d
+size 1735
