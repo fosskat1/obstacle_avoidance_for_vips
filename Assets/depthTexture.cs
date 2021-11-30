3caee2ea36184f4d720b5fa71c1da9a86b16ea09
fosskat1 <katiefos@usc.edu>
2021-11-29 22:16:25 -0800

    both canny and depth working on screen!

--- Assets/depthTexture.cs
+++ Assets/depthTexture.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c4034dfdd60d1109ee9b5742bf4eeb1689eb6f267bcc1e784f321bcb8ced18f
-size 3973
+oid sha256:031aa4b0682fb8cea380e8508de2f1bcbf7a6ed0d364f34558692d5f23255a00
+size 3744
