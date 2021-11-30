89d589b05c90834db60527470d1aab1ed284b13b
fosskat1 <katiefos@usc.edu>
2021-11-29 20:20:02 -0800

    The render texture is the same size as the bounding box images

--- Assets/depthTexture.cs
+++ Assets/depthTexture.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:328fdc636d553c56721baa0495346116119eab9ed9a07d9869f102aed128a91a
-size 781
+oid sha256:e795f6c36fdefc671934b39b7efc57ffdc3b63c9273396cad7db50ef56db9d39
+size 1634
