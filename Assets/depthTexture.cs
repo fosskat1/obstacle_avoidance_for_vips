88ba795fee6573b722ad07f235e0dc4dd130c891
fosskat1 <katiefos@usc.edu>
2021-11-29 22:43:19 -0800

    Fixed image writing issue because of mod %2

--- Assets/depthTexture.cs
+++ Assets/depthTexture.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:031aa4b0682fb8cea380e8508de2f1bcbf7a6ed0d364f34558692d5f23255a00
-size 3744
+oid sha256:50ff638d034db219531288f17de238280ed2e20cee759a32206cda31181bb598
+size 4103
