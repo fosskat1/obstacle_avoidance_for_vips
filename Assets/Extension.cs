a1f8492895296a96bea1e5a8b599d67df640f264
fosskat1 <katiefos@usc.edu>
2021-11-29 21:56:08 -0800

    added canny detection to depth

---
+++ Assets/Extension.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b5a3dd618d6041945c19fb43807187cd939d41b9e56d1a461efeb1fecba117be
+size 475
--- Assets/depthTexture.cs
+++ Assets/depthTexture.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:596cc1d22cabfae4b3d55d33ef669fef57573318814065d2f101be0ef2235428
-size 1937
+oid sha256:9c4034dfdd60d1109ee9b5742bf4eeb1689eb6f267bcc1e784f321bcb8ced18f
+size 3973
