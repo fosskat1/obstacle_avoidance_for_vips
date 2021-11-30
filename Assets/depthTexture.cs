e4da9ea404f92aaa16f0f3d864c547b9bdb43849
fosskat1 <katiefos@usc.edu>
2021-11-29 21:01:41 -0800

    got gitignore working

--- Assets/depthTexture.cs
+++ Assets/depthTexture.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:759b7eeef10db1e323e7fc3a245a283b7562eacd98a6f1da207a387b2ad1db1d
-size 1735
+oid sha256:596cc1d22cabfae4b3d55d33ef669fef57573318814065d2f101be0ef2235428
+size 1937
