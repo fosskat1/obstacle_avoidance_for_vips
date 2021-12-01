da46b4ff8ba9f0c5b4d46bc7fc9b0c490da09b7d
fosskat1 <katiefos@usc.edu>
2021-12-01 13:52:22 -0800

    Stuff is broken

--- Assets/ObjectDetection_Controller.cs
+++ Assets/ObjectDetection_Controller.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:311f7a9ee663b4dc255be64efdab44719740b31281c2fd47819bea4ff94849dd
-size 14453
+oid sha256:fd71fe2f6337157decf623681a2a55be47bde79cc1b54a6b2c70b7c3f6d3de05
+size 17800
