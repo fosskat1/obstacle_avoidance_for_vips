b76539480d72a6c83ab5017c249b4eccf4c82d84
advaitrane <advaitr11@gmail.com>
2021-12-01 14:18:12 -0800

    pre-depth pseudocode

--- Assets/ObjectDetection_Controller.cs
+++ Assets/ObjectDetection_Controller.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd71fe2f6337157decf623681a2a55be47bde79cc1b54a6b2c70b7c3f6d3de05
-size 17800
+oid sha256:e1a8bdd4a57d4bb7db51454064228361872f32e751bb4e01fedd6149ae2d9595
+size 18096
