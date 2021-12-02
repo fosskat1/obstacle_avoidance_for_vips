96c509e8f7d2430e88c002f3a8d9d0fa72317e8e
fosskat1 <katiefos@usc.edu>
2021-12-01 17:16:43 -0800

    Bounding boxes show up on screen

--- Assets/ObjectDetection_Controller.cs
+++ Assets/ObjectDetection_Controller.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4ad96b6e744beb61c840b5928f22e60756918fb70c584eb8c1157f5490098a2
-size 18716
+oid sha256:96c6b09176e93410f7dc7fdb03da811f0fc31e38028e2c0aa2f0f015751d6b3d
+size 18764
