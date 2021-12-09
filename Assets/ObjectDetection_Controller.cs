e2dafeb6a56383288248783e33ecc16f08eee8ff
advaitrane <advaitr11@gmail.com>
2021-12-09 00:05:44 -0800

    working simulation update

--- Assets/ObjectDetection_Controller.cs
+++ Assets/ObjectDetection_Controller.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6137b92226b1b064033f586374e2caafa0a0086e1817ddbff13a8751fc793e0
-size 21435
+oid sha256:fb0a89056e977f7d15c3aa3a83c5a06fa31a896a2862969e7c22d495eb0d8a2d
+size 21427
