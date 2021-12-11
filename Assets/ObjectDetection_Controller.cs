ea135d31bebdece2bdf1f3e8d470c42fc8b762d9
advaitrane <advaitr11@gmail.com>
2021-12-11 09:08:04 -0800

    Added small noise to depth texture

--- Assets/ObjectDetection_Controller.cs
+++ Assets/ObjectDetection_Controller.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58d01b50e0630302ee73ecbfc93d27abb1fb62e458337935b58ebc8ee7a8feb2
-size 21682
+oid sha256:322714399c4446bbe41f93bae47f732a77af7cb560273db768deeb61641e5754
+size 23460
