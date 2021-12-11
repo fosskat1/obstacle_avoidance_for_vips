6bfcd65daaa18f9e46d723032ab5d2dac4289365
fosskat1 <katiefos@usc.edu>
2021-12-10 20:49:14 -0800

    walking around obstacles

--- Assets/ObjectDetection_Controller.cs
+++ Assets/ObjectDetection_Controller.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58d01b50e0630302ee73ecbfc93d27abb1fb62e458337935b58ebc8ee7a8feb2
-size 21682
+oid sha256:2db4e49b523636706fcb743b3f60761cf63b26e83ce251b26db2a9bac4055f01
+size 21685
